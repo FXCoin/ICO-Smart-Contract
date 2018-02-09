70271fd149026d5106e32f4d71ee5f0109d5f7c6
FXCoin <mail2fxglobal@gmail.com>
2018-02-09 12:27:32 +0530

    Update STQToken.js

--- test/STQToken.js
+++ test/STQToken.js
@@ -1,71 +1,249 @@
-'use strict';
+pragma solidity ^0.4.16;
 
-// testrpc has to be run as testrpc -u 0 -u 1
+contract owned {
+    address public owner;
 
-import expectThrow from './helpers/expectThrow';
-import {l, logEvents} from './helpers/debug';
-
-const STQToken = artifacts.require("./STQToken.sol");
-
-
-contract('STQToken', function(accounts) {
-
-    function getRoles() {
-        return {
-            owner3: accounts[0],
-            owner1: accounts[1],
-            owner2: accounts[2],
-            investor1: accounts[2],
-            investor2: accounts[3],
-            investor3: accounts[4],
-            nobody: accounts[5]
-        };
+    function owned() public {
+        owner = msg.sender;
     }
 
-    // converts amount of STQ into STQ-wei
-    function STQ(amount) {
-        return web3.toWei(amount, 'ether');
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
     }
 
-    async function instantiate() {
-        const role = getRoles();
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
 
-        const token = await STQToken.new([role.owner1, role.owner2, role.owner3], {from: role.nobody});
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-        await token.setController(role.owner1, {from: role.owner1});
-        await token.setController(role.owner1, {from: role.owner2});
+contract FXCoin {
+    string public constant _myTokeName = 'FXCoin';//change here
+    string public constant _mySymbol = 'FXC';//change here
+    uint public constant _myinitialSupply = 35000000;//leave it
+    uint8 public constant _myDecimal = 4;//leave it
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
-        await token.mint(role.investor1, STQ(10), {from: role.owner1});
-        await token.mint(role.investor2, STQ(12), {from: role.owner1});
-        await token.disableMinting({from: role.owner1});
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-        await token.startCirculation({from: role.owner1});
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-        return token;
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function FXCoin(
+       
+    ) public {
+        decimals = _myDecimal;
+        totalSupply = _myinitialSupply * (10 ** uint256(_myDecimal));  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = _myTokeName;                                   // Set the name for display purposes
+        symbol = _mySymbol;                               // Set the symbol for display purposes
     }
 
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
 
-    it("test ERC20 is supported", async function() {
-        const role = getRoles();
-        const token = await instantiate();
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
 
-        await token.name({from: role.nobody});
-        await token.symbol({from: role.nobody});
-        await token.decimals({from: role.nobody});
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` in behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
 
-        assert((await token.totalSupply({from: role.nobody})).eq(STQ(22)));
-        assert.equal(await token.balanceOf(role.investor1, {from: role.nobody}), STQ(10));
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public
+        returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
 
-        await token.transfer(role.investor2, STQ(2), {from: role.investor1});
-        assert.equal(await token.balanceOf(role.investor1, {from: role.nobody}), STQ(8));
-        assert.equal(await token.balanceOf(role.investor2, {from: role.nobody}), STQ(14));
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        public
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
 
-        await token.approve(role.investor2, STQ(3), {from: role.investor1});
-        assert.equal(await token.allowance(role.investor1, role.investor2, {from: role.nobody}), STQ(3));
-        await token.transferFrom(role.investor1, role.investor3, STQ(2), {from: role.investor2});
-        assert.equal(await token.allowance(role.investor1, role.investor2, {from: role.nobody}), STQ(1));
-        assert.equal(await token.balanceOf(role.investor1, {from: role.nobody}), STQ(6));
-        assert.equal(await token.balanceOf(role.investor2, {from: role.nobody}), STQ(14));
-        assert.equal(await token.balanceOf(role.investor3, {from: role.nobody}), STQ(2));
-    });
-});
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
+    }
+}
+
+/******************************************/
+/*       ADVANCED TOKEN STARTS HERE       */
+/******************************************/
+
+contract MyAdvancedToken is owned, FXCoin {
+
+
+
+    uint256 public sellPrice;
+    uint256 public buyPrice;
+
+    mapping (address => bool) public frozenAccount;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event FrozenFunds(address target, bool frozen);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function MyAdvancedToken(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) FXCoin() public {}
+
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (balanceOf[_from] >= _value);               // Check if the sender has enough
+        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        require(!frozenAccount[_from]);                     // Check if sender is frozen
+        require(!frozenAccount[_to]);                       // Check if recipient is frozen
+        balanceOf[_from] -= _value;                         // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
+        Transfer(_from, _to, _value);
+    }
+
+    /// @notice Create `mintedAmount` tokens and send it to `target`
+    /// @param target Address to receive the tokens
+    /// @param mintedAmount the amount of tokens it will receive
+    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
+        balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        Transfer(0, this, mintedAmount);
+        Transfer(this, target, mintedAmount);
+    }
+
+    /// @notice `freeze? Prevent | Allow` `target` from sending & receiving tokens
+    /// @param target Address to be frozen
+    /// @param freeze either to freeze it or not
+    function freezeAccount(address target, bool freeze) onlyOwner public {
+        frozenAccount[target] = freeze;
+        FrozenFunds(target, freeze);
+    }
+
+    /// @notice Allow users to buy tokens for `newBuyPrice` eth and sell tokens for `newSellPrice` eth
+    /// @param newSellPrice Price the users can sell to the contract
+    /// @param newBuyPrice Price users can buy from the contract
+    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
+        sellPrice = newSellPrice;
+        buyPrice = newBuyPrice;
+    }
+
+    /// @notice Buy tokens from contract by sending ether
+    function buy() payable public {
+        uint amount = msg.value / buyPrice;               // calculates the amount
+        _transfer(this, msg.sender, amount);              // makes the transfers
+    }
+
+    /// @notice Sell `amount` tokens to contract
+    /// @param amount amount of tokens to be sold
+    function sell(uint256 amount) public {
+        require(this.balance >= amount * sellPrice);      // checks if the contract has enough ether to buy
+        _transfer(msg.sender, this, amount);              // makes the transfers
+        msg.sender.transfer(amount * sellPrice);          // sends ether to the seller. It's important to do this last to avoid recursion attacks
+    }
+}
